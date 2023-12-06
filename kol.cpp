c8e82ff8fc83894ebf627f13a9eb2e63db3abb5a
Konrad <konradjones@outlook.com>
2023-12-07 00:01:12 +0100

    TODO: fast_track, zamkniecie

--- kol.cpp
+++ kol.cpp
@@ -40,17 +40,14 @@ void otwarcie_urzedu(int m){
 interesant *nowy_interesant(int k){
     interesant* new_interesant = (interesant*) malloc(sizeof(interesant));    
     new_interesant->numerek = numerki++;
-    if(kolejki[k]->numerek == 0){
-        new_interesant->przed = kolejki[k]->przed;
-        new_interesant->po = kolejki[k];
+    new_interesant->przed = kolejki[k]->przed;
+    new_interesant->po = kolejki[k];
+
+    if(kolejki[k]->przed->po == kolejki[k])
         kolejki[k]->przed->po = new_interesant;
-        kolejki[k]->przed = new_interesant;
-    }else{
-        new_interesant->po = kolejki[k]->po;
-        new_interesant->przed = kolejki[k];
-        kolejki[k]->po->przed = new_interesant;
-        kolejki[k]->po = new_interesant;
-    }
+    else
+        kolejki[k]->przed->przed = new_interesant;
+    kolejki[k]->przed = new_interesant;
     return new_interesant;
 }
 
@@ -61,47 +58,93 @@ int numerek(interesant *i){
 interesant *obsluz(int k){
     if(kolejki[k]->po == kolejki[k]->przed)
         return NULL;
-    if(kolejki[k]->numerek == 0){
-        interesant* obsluzony = kolejki[k]->po;
-        kolejki[k]->po = obsluzony->po;
-        kolejki[k]->po->przed = kolejki[k];
-    }else{
-        interesant* obsluzony = kolejki[k]->przed;
-        kolejki[k]->przed = obsluzony->przed;
-        kolejki[k]->przed->po = kolejki[k]; 
-    }
+    interesant* obsluzony = kolejki[k]->po;
+    interesant* nast = nastepny(obsluzony, kolejki[k]);
+    if(nast->po != obsluzony)
+        nast->przed = kolejki[k];
+    else
+        nast->po = kolejki[k];
+    kolejki[k]->po = nast;
     return obsluzony;
 }
-// -------------------------------------------------
+
 void zmiana_okienka(interesant *i, int k){
-    i->przed->po = i->po;
-    i->po->przed = i->przed;
-    kolejki[k]->przed->po = i;
+    interesant* poprzedni = i->przed;
+    interesant* nastepny = i->po;
+
+    if(nastepny->przed == i)
+        nastepny->przed = poprzedni;
+    else
+        nastepny->po = poprzedni;
+
+    if(poprzedni->po == i)
+        nastepny->po = poprzedni;
+    else
+        nastepny->przed = poprzedni;
+
+    if(kolejki[k]->przed->po == kolejki[k])
+        kolejki[k]->przed->po = i;
+    else
+        kolejki[k]->przed->przed = i;
     kolejki[k]->przed = i;
 }
 
 void zamkniecie_okienka(int k1, int k2){
-    kolejki[k2]->przed->po = kolejki[k1]->po;
+    if(kolejki[k2]->przed->po == kolejki[k2])
+        kolejki[k2]->przed->po = kolejki[k1]->po;
+    else
+        kolejki[k2]->przed->przed = kolejki[k1]->po;
+
     kolejki[k2]->przed = kolejki[k1]->przed;
     kolejki[k1]->przed = kolejki[k1];
     kolejki[k1]->po = kolejki[k1];
 }
 
+// -------------------------------------------------
 std::vector<interesant *> fast_track(interesant *i1, interesant *i2){
+    interesant* lewy1 = i1->przed;
+    interesant* lewy2 = i1;
+    interesant* prawy1 = i1->po;
+    interesant* prawy2 = i1;
     std::vector<interesant*> obsluzeni;
-    interesant* temp = i1->przed;
-    while(i1->przed != i2){
-        printf("%d\n", i1->numerek);
-        obsluzeni.push_back(i1);
-        i1 = i1->po;
+    obsluzeni.push_back(i1);
+
+    while(lewy1 != i2 && prawy1 != i2){
+        interesant* temp = lewy1;
+        lewy1 = nastepny(lewy2, lewy1);
+        lewy2 = temp;
+
+        temp = prawy1;
+        prawy1 = nastepny(prawy1, prawy2);
+        prawy2 = temp;
     }
-    i1->przed = temp;
-    temp->po = i1;
+    if(lewy1 == i2){
+        lewy2 = i1;
+        lewy1 = i1->przed;
+        while(lewy1 != i2){
+            interesant* temp = lewy1;
+            lewy1 = nastepny(lewy2, lewy1);
+            lewy2 = temp;
+            obsluzeni.push_back(lewy2);
+        }
+    }else{
+        prawy1 = i1->po;
+        prawy2 = i1;
+        while(prawy1 != i2){
+            interesant* temp = prawy1;
+            prawy1 = nastepny(prawy1, prawy2);
+            prawy2 = temp;
+            obsluzeni.push_back(prawy2);
+        }
+    }
+    obsluzeni.push_back(i2);
     return obsluzeni;
 }
 
 void naczelnik(int k){
-    kolejki[k]->numerek = -1;
+    interesant* temp = kolejki[k]->po;
+    kolejki[k]->po = kolejki[k]->przed;
+    kolejki[k]->przed = temp;
 }
 
 std::vector<interesant *> zamkniecie_urzedu(){
